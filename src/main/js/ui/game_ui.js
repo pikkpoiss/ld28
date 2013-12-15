e49ea88bc279629429b67ac57af371e4925bf431
Arne Roomann-Kurrik <kurrik@gmail.com>
2013-12-14 17:11:19 -0800

    Added angular.

--- src/main/js/ui/game_ui.js
+++ src/main/js/ui/game_ui.js
@@ -2,8 +2,9 @@ define(function (require) {
   var defineComponent = require('flight/lib/component');
 
   function GameUI() {
-    this.render = function render(game) {
+    this.render = function render(evt, game) {
       console.log(game);
+      ngGameStateAPI.setGame(game);
     };
 
     this.after('initialize', function() {
