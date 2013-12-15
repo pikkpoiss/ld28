a554707cc295c59cd9c58bf73819b892db2d7ca1
Arne Roomann-Kurrik <kurrik@gmail.com>
2013-12-14 18:20:12 -0800

    More UI work.

--- src/main/js/ui/game_ui.js
+++ src/main/js/ui/game_ui.js
@@ -7,8 +7,35 @@ define(function (require) {
       ngGameStateAPI.setGame(game);
     };
 
+    this.onDragStart = function(evt) {
+      console.log(this, evt);
+      evt.target.style.opacity = '0.4';
+      evt.originalEvent.dataTransfer.effectAllowed = 'move';
+      evt.originalEvent.dataTransfer.setData('text/html', evt.target.innerHTML);
+    };
+
+    this.onDragOver = function(evt) {
+      if (evt.preventDefault) {
+        evt.preventDefault();
+      }
+      evt.originalEvent.dataTransfer.dropEffect = 'move';
+      return false;
+    };
+
+    this.onDragEnter = function(evt) {
+      evt.target.classList.add('over');
+    };
+
+    this.onDragLeave = function(evt) {
+      evt.target.classList.remove('over');
+    };
+
     this.after('initialize', function() {
       this.on('changeState', this.render);
+      this.on('dragstart', this.onDragStart);
+      this.on('dragenter', this.onDragEnter);
+      this.on('dragover', this.onDragOver);
+      this.on('dragleave', this.onDragLeave);
     });
   }
 
